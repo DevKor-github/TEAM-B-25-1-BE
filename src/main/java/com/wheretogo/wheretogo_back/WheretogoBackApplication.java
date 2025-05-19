98a84e7bf87d6f3d76218aa0a25ea05951c84dc9
leesanghuu <seoduuuu@naver.com>
2025-05-19 15:33:23 +0900

    Initial commit

---
+++ src/main/java/com/wheretogo/wheretogo_back/WheretogoBackApplication.java
@@ -0,0 +1,13 @@
+package com.wheretogo.wheretogo_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheretogoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheretogoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wheretogo/wheretogo_back/WheretogoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wheretogo.wheretogo_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheretogoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
