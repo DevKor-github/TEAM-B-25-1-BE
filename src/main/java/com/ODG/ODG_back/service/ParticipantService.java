3a81fcee50eb0d06c2527e09de2b330be724af8a
hodomaroo <enghemdwkd@gmail.com>
2025-06-08 00:07:55 +0900

    Feat : Participant Service 구현 완료

--- src/main/java/com/ODG/ODG_back/service/ParticipantService.java
+++ src/main/java/com/ODG/ODG_back/service/ParticipantService.java
@@ -1,15 +1,12 @@
 package com.ODG.ODG_back.service;
 
-import com.ODG.ODG_back.dto.participant.request.ParticipantAdministrationRequestDto;
 import com.ODG.ODG_back.dto.participant.request.ParticipantDeletionRequestDto;
-import com.ODG.ODG_back.dto.participant.request.ParticipantModificationRequestDto;
-import com.ODG.ODG_back.dto.participant.response.ParticipantResponseDto;
+import com.ODG.ODG_back.dto.participant.request.ParticipantDto;
 
 import java.util.List;
 
 public interface ParticipantService {
-    ParticipantResponseDto addParticipant(String linkCode, ParticipantAdministrationRequestDto dto);
-    void modifyParticipant(String linkCode, ParticipantModificationRequestDto dto);
+    void addParticipant(String linkCode, ParticipantDto dto);
+    void modifyParticipant(String linkCode, ParticipantDto dto);
     void deleteParticipant(String linkCode, ParticipantDeletionRequestDto dto);
-    List<ParticipantResponseDto> getParticipants(String linkCode);
 }
