4473cace7d8be4d27b86d8952c429d0ada40e261
takmj00 <takmj00@gmail.com>
2025-06-21 16:13:30 +0900

    fix: 타입 수정

--- src/main/java/com/ODG/ODG_back/dto/participant/response/ParticipantListResponseDto.java
+++ src/main/java/com/ODG/ODG_back/dto/participant/response/ParticipantListResponseDto.java
@@ -3,12 +3,14 @@ package com.ODG.ODG_back.dto.participant.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.math.BigDecimal;
+
 @Getter
 @AllArgsConstructor
 public class ParticipantListResponseDto {
     private Long participantId;
     private String name;
     private String transportType;
-    private Double lat;
-    private Double lng;
+    private BigDecimal lat;
+    private BigDecimal lng;
 }
