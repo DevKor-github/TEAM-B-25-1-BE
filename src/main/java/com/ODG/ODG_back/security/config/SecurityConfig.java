e4ecbe52e6d1b3d3150d72a39c68aa1cecb1d2b6
leesanghuu <seoduuuu@naver.com>
2025-07-03 11:57:51 +0900

    feaure: config, jwt 키 추가

--- src/main/java/com/ODG/ODG_back/security/config/SecurityConfig.java
+++ src/main/java/com/ODG/ODG_back/security/config/SecurityConfig.java
@@ -1,4 +1,35 @@
 package com.ODG.ODG_back.security.config;
 
+import com.ODG.ODG_back.security.jwt.JwtTokenFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtTokenFilter jwtTokenFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/participants/**").authenticated()
+                        .requestMatchers("/auth/**", "meetings/**").permitAll()
+                        .anyRequest().permitAll()
+                )
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 }
