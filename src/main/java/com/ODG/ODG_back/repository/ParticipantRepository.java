fb53b1dd9e88ae555a8b1b40f327c9b823874f0c
takmj00 <takmj00@gmail.com>
2025-07-23 00:03:28 +0900

    refactor: vote jwt 인증 추가

--- src/main/java/com/ODG/ODG_back/repository/ParticipantRepository.java
+++ src/main/java/com/ODG/ODG_back/repository/ParticipantRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
     Optional<Participant> findByUserIdAndMeeting(String userId, Meeting meeting);
+    Optional<Participant> findByUserId(String userId);
 }
