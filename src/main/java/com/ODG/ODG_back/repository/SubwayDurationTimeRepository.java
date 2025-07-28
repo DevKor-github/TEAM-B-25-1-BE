5928f80f96bd6c0a22c2900eda2fea28fd284e4b
hodomaroo <enghemdwkd@gmail.com>
2025-07-29 03:10:42 +0900

    feat: 지하철 역간 이동시간 Repository 추가

---
+++ src/main/java/com/ODG/ODG_back/repository/SubwayDurationTimeRepository.java
@@ -0,0 +1,10 @@
+package com.ODG.ODG_back.repository;
+
+import com.ODG.ODG_back.domain.SubwayDurationTime;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SubwayDurationTimeRepository extends JpaRepository<SubwayDurationTime, Long> {
+   Optional<SubwayDurationTime> findByStartAndEnd(Long start, Long end);
+}
