bf26b0116f2721e23ca3a77660cd437e1c425cf1
leesanghuu <seoduuuu@naver.com>
2025-08-24 02:29:57 +0900

    chore: swagger에 local 서버 추가

--- src/main/java/com/ODG/ODG_back/swagger/SwaggerConfig.java
+++ src/main/java/com/ODG/ODG_back/swagger/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -33,6 +34,9 @@ public class SwaggerConfig {
                 .components(components)
                 .addServersItem(new io.swagger.v3.oas.models.servers.Server()
                         .url("https://o-digo.com")
-                        .description("Production server"));
+                        .description("Production server"))
+                .addServersItem(new Server()
+                        .url("http://localhost:8080")
+                        .description("Local server"));
     }
 }
